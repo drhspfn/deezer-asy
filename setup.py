eddcbc1a10633584cdfefd1711eb4466d87dea94
drhspfn <63609847+drhspfn@users.noreply.github.com>
2023-07-12 15:58:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 import setuptools
 
-
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='pydeezer_asy',
-    version='1.2.0',
+    version='1.2.1',
     description='Asynchronous version of the `py-deezer` module',
     author='drhspfn',
+    author_email="drhspfn@gmail.com",
     packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drhspfn/deezer-asy",
     install_requires=[
         "aiofiles",
         "cryptography",
@@ -20,4 +25,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"
     ],
+
 )
